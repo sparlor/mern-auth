ce238e763e52fad6840e6531066e13c1d46c0342
Scotty Parlor <sparlor91@gmail.com>
2019-10-18 09:10:12 -0400

    evr name change for services

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Route} from "react-router-dom";
+
+import { Provider } from 'react-redux';
+import store from './store';
+
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+
+function App() {
+  return (
+    <Provider store = { store }>
+      <Router>
+        <div className="App">
+          <Navbar/>
+          <Route exact path="/" component={Landing}/>
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </div>
+      </Router>
+    </Provider>
+  );
+}
+
+export default App;
---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: "mongodb+srv://admin:mYk2FAkkfzMxWwzA@cluster0-cw5om.mongodb.net/test?retryWrites=true&w=majority",
+    secretOrKey: "secret"
+};
